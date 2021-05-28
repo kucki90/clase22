0793423d26efdd5411646ae1ac67e1e873d6052f
kucki90 <c.kuckiewicz@hotmail.com>
2021-05-28 15:25:03 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require('express'); //requiero express
+const app = express();//requiero la funcionalidad de express
+
+app.listen(3000, () => console.log('Servidor funcionando en el puerto 3000'))
